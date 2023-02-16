3eadf03b91575001ff70f093f4092f13adca9540
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-02-16 14:31:05 -0600

    Initial Commit

---
+++ montecarloOrbitals/montecarloOrbitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  montecarloOrbitalsApp.swift
+//  montecarloOrbitals
+//
+//  Created by IIT PHYS 440 on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct montecarloOrbitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
